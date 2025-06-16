2b9065c16073a495e221d00cfb20f14241843a9a
Avanish697 <analystavanish@gmail.com>
2025-06-16 11:22:07 +0000

    Server.py

--- Server.py
+++ Server.py
@@ -1,11 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '✅ Flask server is accessible via IP!'
+    return '✅ Flask server is accessible via Render!'
 
 if __name__ == '__main__':
-    # Listen on ALL network interfaces
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))  # default for local, uses Render's assigned port if deployed
+    app.run(host='0.0.0.0', port=port)
